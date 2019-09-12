811ceeb2327714846ad231df64c86ba5f6b4a3e9
Maheshwari2604 <maheshwarishivam2604@gmail.com>
2019-09-12 17:03:26 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render , HttpResponse
+
+# Create your views here.
+def azure_test(request):
+    return HttpResponse("hey")
